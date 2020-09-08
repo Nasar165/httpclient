b9ae2a24da45e85fd4f5d760f7262584e447cb5d
Nasar Eddaoui <nasar165@hotmail.com>
2020-09-08 15:01:29 +0700

    network unreachable error

--- src/core/dns.c
+++ src/core/dns.c
@@ -1,9 +1,10 @@
-#include <stdio.h>           // NULL
-#include <netdb.h>           // hostent addrinfo
-#include <string.h>          // strcpy
-#include <arpa/inet.h>       // inet_ntoa
-#include <sys/socket.h>      // socket sendto perror
-#include <unistd.h>          // close
+#include <stdio.h>      // NULL
+#include <netdb.h>      // hostent addrinfo
+#include <string.h>     // strcpy
+#include <arpa/inet.h>  // inet_ntoa
+#include <sys/socket.h> // socket sendto perror
+#include <unistd.h>     // close
+#include <errno.h>
 #include "../common/error.h" // error
 
 // DNS SERVERS LISTENS ON PORT 53
@@ -21,11 +22,11 @@ int resolve(char *domain, char *ip)
     struct addrinfo hints, *result;
 
     memset(&hints, 0, sizeof(hints));
-    hints.ai_family = AF_UNSPEC;
+    hints.ai_family = AF_INET;      // Use only IPV4 to avoid network unreachable error
     hints.ai_socktype = SOCK_DGRAM; // Use UDP PROTOCOL to connect to the DNS on port 53
 
     err = getaddrinfo(domain, SERVICE, &hints, &result);
-    if (err != 0)
+    if (err != 0 || errno != 0)
         return error("An error has occurred while getting address. %s.", gai_strerror(err));
 
     sock = socket(result->ai_family, result->ai_socktype, 0);
